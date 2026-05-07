32e3ce8f2ca8be1f8a09cb4c6cc95966ed6316a9
Ngocthao <nguyenthao.ntnt2020@gmail.com>
2026-05-07 12:22:23 +0700

    create entity

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,40 @@
 package entity;
 
-public class Entity {
+import java.awt.Graphics;
+public abstract class Entity {
+    protected int x;
+    protected int y;
+    protected int speed;
+
+    public Entity( int x, int y, int speed ){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    public abstract void update();
+
+    public abstract void draw(Graphics g);
+
+    public int getX() {
+         return x;
+    }
     
+    public int getY() {
+        return y;
+    }
+
+    public void setPosition( int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setSpeed( int speed){
+        this.speed = speed;
+    }
+
 }
