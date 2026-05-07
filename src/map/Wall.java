53adaad6f506c0b773f1e638414768940c154b29
Ngocthao <nguyenthao.ntnt2020@gmail.com>
2026-05-07 12:31:47 +0700

    create wall

--- src/map/Wall.java
+++ src/map/Wall.java
@@ -1,1 +1,34 @@
 package map;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Wall {
+    private int x;
+    private int y;
+    private int size;
+
+    public Wall( int x, int y, int size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public void draw(Graphics g){
+        g.setColor(Color.PINK);
+        g.fillRect(x, y, size, size);
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getSize(){
+        return size;
+    }
+   
+}
