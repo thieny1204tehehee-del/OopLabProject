ca141c3ac3c4c2d16e49831267b86085b5b41bb8
Thien Y <thieny1204tehehee@gmail.com>
2026-05-08 15:10:06 +0700

    create ghost and RandomMovement

--- src/entity/Ghost.java
+++ src/entity/Ghost.java
@@ -1,1 +1,63 @@
 package entity;
+
+import strategy.MovementStrategy;
+import game.GameConfig;
+import map.Map;
+
+import java.util.Random;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Ghost extends Entity {
+    private Direction direction;
+    private int size;
+    private MovementStrategy strategy;
+    private Random random;
+    private Map map;
+
+    public Ghost(int x, int y, int size, MovementStrategy strategy, Map map) {
+        super(x, y,GameConfig.GHOST_SPEED);
+        this.size = size;
+        this.map = map;
+        this.strategy = strategy;
+        this.random = new Random();
+        this.direction = Direction.values()[random.nextInt(Direction.values().length)];
+    }
+
+    public void update() {
+        move();
+    }
+
+    public void move() {
+        strategy.move(this);
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.MAGENTA);
+        g.fillOval(x, y, size, size);
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public void setStrategy(MovementStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public MovementStrategy getStrategy() {
+        return strategy;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+}
--- src/strategy/RandomMovement.java
+++ src/strategy/RandomMovement.java
@@ -1,1 +1,9 @@
 package strategy;
+
+import entity.Ghost;
+
+public class RandomMovement implements MovementStrategy {
+    public void move(Ghost ghost) {
+        
+    }
+}
