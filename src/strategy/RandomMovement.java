86ae5319808f960ba5740a44cb72273879190b30
Thien Y <thieny1204tehehee@gmail.com>
2026-05-10 02:01:08 +0700

    create RandomMovement

--- src/strategy/RandomMovement.java
+++ src/strategy/RandomMovement.java
@@ -1,9 +1,55 @@
 package strategy;
 
 import entity.Ghost;
+import game.GameConfig;
+import map.Map;
+import entity.Direction;
+import java.util.Random;
 
 public class RandomMovement implements MovementStrategy {
+        private Random random = new Random();
+        private Direction[] dir = Direction.values();
+
     public void move(Ghost ghost) {
-        
+            int x = ghost.getX();
+            int y = ghost.getY();
+            int speed = ghost.getSpeed();
+            Map map = ghost.getMap();
+            int tileSize = map.getTileSize();
+            int size = ghost.getSize();
+
+            
+            int nextX = x;
+            int nextY = y;
+    
+            switch (ghost.getDirection()) {
+                case UP:
+                    nextY -= speed;
+                    break;
+                case DOWN:
+                    nextY += speed;
+                    break;
+                case LEFT:
+                    nextX -= speed;
+                    break;
+                case RIGHT:
+                    nextX += speed;
+                    break;
+            }
+
+            boolean hitWall = map.isWall(nextX/tileSize, nextY/tileSize) 
+                            || map.isWall(nextX + size - 1/tileSize, nextY/tileSize) 
+                            || map.isWall(nextX/tileSize, nextY + tileSize - 1/tileSize) 
+                            || map.isWall(nextX + tileSize - 1/tileSize, nextY + tileSize - 1/tileSize);
+
+            if (hitWall == false) {
+                ghost.setPosition(nextX, nextY);
+            } else {
+                ghost.setDirection(dir[random.nextInt(dir.length)]);
+            }
+
+            if (random.nextInt(100) < GameConfig.GHOST_TURN_CHANCE) {
+                ghost.setDirection(dir[random.nextInt(dir.length)]);
+            }
     }
 }
