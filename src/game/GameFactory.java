9160648898b61eb0343b2e48641c82a2eabe0990
Ngocthao <nguyenthao.ntnt2020@gmail.com>
2026-05-11 08:56:39 +0700

    create gamefactory

--- src/game/GameFactory.java
+++ src/game/GameFactory.java
@@ -1,1 +1,41 @@
 package game;
+
+import entity.Player;
+import entity.Ghost;
+import map.Map;
+import strategy.RandomMovement;
+
+public class GameFactory {
+
+    public static Map createMap() {
+        int[][] grid = {
+            {1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,1,0,0,0,0,0,1},
+            {1,0,1,1,0,1,1,1,0,1},
+            {1,0,0,0,0,0,0,1,0,1},
+            {1,0,1,1,1,1,0,1,0,1},
+            {1,0,0,0,0,1,0,0,0,1},
+            {1,1,1,1,0,1,1,1,0,1},
+            {1,0,0,1,0,0,0,1,0,1},
+            {1,0,0,0,0,0,0,0,0,1},
+            {1,1,1,1,1,1,1,1,1,1}
+        };
+        return new Map(grid);
+    }
+
+     public static Player createPlayer(Map map) {
+        return new Player(
+            GameConfig.TILE_SIZE,
+            GameConfig.TILE_SIZE,
+            GameConfig.TILE_SIZE,
+            map
+        );
+    }
+
+    public static Ghost createGhost(Map map) {
+        int[] pos = map.getRandomEmptyTile();
+        int x = pos[0] * map.getTileSize();
+        int y = pos[1] * map.getTileSize();
+        return new Ghost(x, y, GameConfig.GHOST_SIZE, new RandomMovement(), map);
+    }
+}
