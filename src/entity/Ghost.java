dce75decdf26628d87408026e80b9c0d0e116c00
Thien Y <thieny1204tehehee@gmail.com>
2026-05-17 02:28:13 +0700

    update image

--- src/entity/Ghost.java
+++ src/entity/Ghost.java
@@ -5,13 +5,16 @@ import map.Map;
 
 import java.util.Random;
 import java.awt.Graphics;
-import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Ghost extends Entity {
     private Direction direction;
     private MovementStrategy strategy;
     private Random random;
     private Map map;
+    private BufferedImage leftImg, rightImg;
 
     public Ghost(int x, int y,int speed, int size, MovementStrategy strategy, Map map) {
         super(x, y, speed, size);
@@ -19,7 +22,15 @@ public class Ghost extends Entity {
         this.strategy = strategy;
         this.random = new Random();
         this.direction = Direction.values()[random.nextInt(Direction.values().length)];
+
+        try {
+            leftImg = ImageIO.read(getClass().getResource("/ghost_left.png"));
+            rightImg = ImageIO.read(getClass().getResource("/ghost_right.png"));
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
     }
+    
 
     @Override
     public void update() {
@@ -28,8 +39,11 @@ public class Ghost extends Entity {
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.CYAN);
-        g.fillOval(x, y, size, size);
+        if (direction == Direction.LEFT) {
+            g.drawImage(leftImg, x, y, size, size, null);
+        } else {
+            g.drawImage(rightImg, x, y, size, size, null);
+        }
     }
 
     private void move() {
