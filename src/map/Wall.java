776042fa686358d3350370b28f983c03b5e47f67
Ngocthao <nguyenthao.ntnt2020@gmail.com>
2026-05-19 19:53:19 +0700

    update wall

--- src/map/Wall.java
+++ src/map/Wall.java
@@ -1,22 +1,31 @@
 package map;
 
-import java.awt.Color;
+
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 public class Wall {
     private int x;
     private int y;
     private int size;
+    private BufferedImage image;
 
     public Wall( int x, int y, int size){
         this.x = x;
         this.y = y;
         this.size = size;
+
+        try {
+            image = ImageIO.read(getClass().getResource("/bush_tile.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void draw(Graphics g){
-        g.setColor(Color.PINK);
-        g.fillRect(x, y, size, size);
+        g.drawImage(image, x, y, size, size, null);
     }
 
     public int getX(){
